bcaa387af0ced404d11d3a83ea6f0bd940dcd025
h24marni <nisser12@outlook.com>
2025-12-07 18:29:54 +0100

    fsadasd

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+//checkbox hämta från getElementById
+const checkbox = document.getElementById("divStyle");
 
+//textfält hämta från getElementsByClassName
+const textFields = document.getElementsByClassName("textfield");
+
+//knapp hämtas via querySelector
+const removeButton = document.querySelector("#removeBtn");
+
+//hämta Div via getElementById
+const outputDiv = document.getElementById("outputDiv");
